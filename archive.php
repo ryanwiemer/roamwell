26ba3df6f502d8d279f2fb1e2f5569c2e7dc5ab7
Ryan Wiemer <ryan@ryanwiemer.com>
2016-03-10 00:18:39 -0800

    Playing more with color and Category archive styling

--- archive.php
+++ archive.php
@@ -7,41 +7,36 @@ get_header(); ?>
 <div class="main">
 	<?php if ( have_posts() ) : ?>
 		<header class="page__header archive__header">
-			<h2 class="page__title">
 				 <?php
             if ( is_category() ) {
-                printf( __( 'Category: %s' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-
+                printf( __( '<div class="page__title page__title--category">%s' ), '<span>' . single_cat_title( '', false ) . '</span></h2>' );
+								echo category_description( $category_id ); printf(__('</div>'));
             } elseif ( is_tag() ) {
-                printf( __( 'Tag: %s' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+                printf( __( '<h2 class="page__title">Tag: %s' ), '<span>' . single_tag_title( '', false ) . '</span></h2>' );
 
             } elseif ( is_author() ) {
                 /* Queue the first post, that way we know
                  * what author we're dealing with (if that is the case).
                 */
                 the_post();
-                printf( __( 'Author: %s' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+                printf( __( '<h2 class="page__title">Author: %s' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span></h2>' );
                 /* Since we called the_post() above, we need to
                  * rewind the loop back to the beginning that way
                  * we can run the loop properly, in full.
                  */
                 rewind_posts();
-
             } elseif ( is_day() ) {
-                printf( __( 'Daily Archives: %s' ), '<span>' . get_the_date() . '</span>' );
+                printf( __( '<h2 class="page__title">Daily Archives: %s' ), '<span>' . get_the_date() . '</span></h2>' );
 
             } elseif ( is_month() ) {
-                printf( __( 'Monthly Archives: %s' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+                printf( __( '<h2 class="page__title">Monthly Archives: %s' ), '<span>' . get_the_date( 'F Y' ) . '</span></h2>' );
 
             } elseif ( is_year() ) {
-                printf( __( 'Yearly Archives: %s' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-
+                printf( __( '<h2 class="page__title">Yearly Archives: %s' ), '<span>' . get_the_date( 'Y' ) . '</span></h2>' );
             } else {
                 _e( 'Archives' );
-
             }
         ?>
-			</h2>
 		</header>
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
