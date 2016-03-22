0697071251bbd6557ad0d8fc8787dcf01fdb5c73
Ryan Wiemer <ryan@ryanwiemer.com>
2016-03-21 20:02:11 -0700

    Minor fixes to ol and ul styling. Also hiding the website option on the comments.

--- content-page.php
+++ content-page.php
@@ -3,11 +3,11 @@
 	* Content-Pgae
 	*/
 ?>
-	<header class="page__header">
-		<h2 class="page__title"><?php the_title(); ?></h2>
-	</header>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="post__content">
-			<?php the_content(); ?>
-		</div>
-	</article>
+<header class="page__header">
+	<h2 class="page__title"><?php the_title(); ?></h2>
+</header>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="post__content">
+		<?php the_content(); ?>
+	</div>
+</article>
