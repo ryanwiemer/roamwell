4a8aca231e75b3494ee5754900b17b064dc37d7c
Ryan Wiemer <ryan@ryanwiemer.com>
2016-03-24 15:54:45 -0700

    Adding Google Analytics

--- header.php
+++ header.php
@@ -15,7 +15,16 @@
   </script>
   <script type="text/javascript" src="<?php echo get_site_url(); ?>/wp-content/themes/roamwell/build/js/vendor/fonts.min.js"></script>
   <?php wp_head(); ?>
-  <!-- Google Analytics -->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75580000-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body <?php body_class(); ?>>
   <?php get_search_form(); ?>
